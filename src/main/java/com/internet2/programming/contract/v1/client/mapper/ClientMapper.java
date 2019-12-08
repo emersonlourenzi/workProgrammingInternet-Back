28a58d8c5f874901665afff3aa81453aad7d652c
emerson <emersonl.lourenzi@gmail.com>
2019-12-08 20:09:28 -0300

    inseridos classes estaticas para mapeamento das requisições.

--- src/main/java/com/internet2/programming/contract/v1/client/mapper/ClientMapper.java
+++ src/main/java/com/internet2/programming/contract/v1/client/mapper/ClientMapper.java
@@ -1,4 +1,24 @@
 package com.internet2.programming.contract.v1.client.mapper;
 
+import com.internet2.programming.contract.v1.client.model.ClientRequest;
+import com.internet2.programming.contract.v1.client.model.ClientResponse;
+import com.internet2.programming.implementation.client.model.ClientModelImp;
+
 public class ClientMapper {
+    public static ClientResponse mapToContract(ClientModelImp clientModelImp) {
+        return ClientResponse.builder()
+                .id(clientModelImp.getId())
+                .nameClient(clientModelImp.getNameClient())
+                .foneClient(clientModelImp.getFoneClient())
+                .addressClient(clientModelImp.getAddressClient())
+                .build();
+    }
+
+    public static ClientModelImp mapToImp(ClientRequest clientRequest) {
+        return ClientModelImp.builder()
+                .nameClient(clientRequest.getNameClient())
+                .foneClient(clientRequest.getFoneClient())
+                .addressClient(clientRequest.getAddressClient())
+                .build();
+    }
 }
--- src/main/java/com/internet2/programming/implementation/client/mapper/ClientMapperImp.java
+++ src/main/java/com/internet2/programming/implementation/client/mapper/ClientMapperImp.java
@@ -1,4 +1,24 @@
 package com.internet2.programming.implementation.client.mapper;
 
+import com.internet2.programming.implementation.client.model.ClientModelImp;
+import com.internet2.programming.implementation.client.repository.ClientEntity;
+
 public class ClientMapperImp {
+    public static ClientModelImp mapToModel(ClientEntity clientEntity) {
+        return ClientModelImp.builder()
+                .id(clientEntity.getId())
+                .nameClient(clientEntity.getNameClient())
+                .foneClient(clientEntity.getFoneClient())
+                .addressClient(clientEntity.getAddressClient())
+                .build();
+    }
+
+    public static ClientEntity mapToEntity(ClientModelImp clientModelImp) {
+        return ClientEntity.builder()
+                .id(clientModelImp.getId())
+                .nameClient(clientModelImp.getNameClient())
+                .foneClient(clientModelImp.getFoneClient())
+                .addressClient(clientModelImp.getAddressClient())
+                .build();
+    }
 }
